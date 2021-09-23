8ad6894d468e26ab6f34f9d1e8c628e3203113b5
Feodor0090 <79257300125@ya.ru>
2021-09-23 20:40:05 +0300

    Refactor `View`

--- src/njtai/ui/View.java
+++ src/njtai/ui/View.java
@@ -99,6 +99,10 @@ public class View extends Canvas implements Runnable {
 	}
 
 	private void resize(int size) {
+		toDraw = null;
+		System.gc();
+		repaint();
+		
 		int h = getHeight();
 		int w = (int) (((float) h / origImg.getHeight()) * origImg.getWidth());
 
@@ -135,43 +139,9 @@ public class View extends Canvas implements Runnable {
 			g.fillRect(0, 0, getWidth(), getHeight());
 
 			if (toDraw == null) {
-				String info;
-				if (error) {
-					info = "Failed to load image.";
-				} else if (emo.infoReady == -1) {
-					info = "Failed to fetch pages.";
-				} else if (emo.infoReady == -2) {
-					info = "Waiting loader...";
-				} else {
-					info = origImg == null ? "Loading image..." : "Resizing...";
-				}
-				g.setGrayScale(255);
-				g.drawString(info, getWidth() / 2, getHeight() / 2, Graphics.HCENTER | Graphics.TOP);
-				if (hasPointerEvents()) {
-					int fh = f.getHeight();
-					// grads
-					fillGrad(g, getWidth() * 2 / 3, getHeight() - 50, getWidth() / 3, 51, 0, 0x222222);
-					// lines
-					g.setGrayScale(255);
-					g.drawLine(getWidth() * 2 / 3, getHeight() - 50, getWidth(), getHeight() - 50);
-					g.drawLine(getWidth() * 2 / 3, getHeight() - 50, getWidth() * 2 / 3, getHeight());
-					// captions
-					g.setGrayScale(255);
-					g.drawString(touchCaps[5], getWidth() * 5 / 6, getHeight() - 25 - fh / 2,
-							Graphics.TOP | Graphics.HCENTER);
-
-				}
+				paintNullImg(g, f);
 			} else {
-				int hw = toDraw.getWidth() / 2;
-				int hh = toDraw.getHeight() / 2;
-				if (x < -hw)
-					x = -hw;
-				if (x > hw)
-					x = hw;
-				if (y < -hh)
-					y = -hh;
-				if (y > hh)
-					y = hh;
+				limitOffset();
 				if (zoom != 1) {
 					g.drawImage(toDraw, x + getWidth() / 2, y + getHeight() / 2, Graphics.HCENTER | Graphics.VCENTER);
 				} else {
@@ -184,33 +154,7 @@ public class View extends Canvas implements Runnable {
 					drawTouchControls(g, f);
 				}
 			}
-			String pageNum = (page + 1) + "/" + emo.pages;
-			String zoomN = "x" + zoom;
-			String prefetch = (emo.infoReady >= 0 && emo.infoReady < 100) ? ("fetching pages " + emo.infoReady + "%")
-					: null;
-			String ram;
-			{
-				long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-				used /= 1024;
-				if (used <= 4096) {
-					ram = used + "kb";
-				} else {
-					ram = (used / 1024) + "mb";
-				}
-			}
-			g.setGrayScale(0);
-			g.fillRect(0, 0, f.stringWidth(pageNum), f.getHeight());
-			g.fillRect(getWidth() - f.stringWidth(zoomN), 0, f.stringWidth(zoomN), f.getHeight());
-			if (prefetch != null)
-				g.fillRect(0, getHeight() - f.getHeight(), f.stringWidth(pageNum), f.getHeight());
-			g.fillRect(getWidth() - f.stringWidth(ram), getHeight() - f.getHeight(), f.stringWidth(ram), f.getHeight());
-
-			g.setGrayScale(255);
-			g.drawString(pageNum, 0, 0, 0);
-			g.drawString(zoomN, getWidth() - f.stringWidth(zoomN), 0, 0);
-			if (prefetch != null)
-				g.drawString(prefetch, 0, getHeight() - f.getHeight(), 0);
-			g.drawString(ram, getWidth(), getHeight(), Graphics.BOTTOM | Graphics.RIGHT);
+			paintHUD(g, f);
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -219,6 +163,78 @@ public class View extends Canvas implements Runnable {
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
+		}
+	}
+
+	private void paintHUD(Graphics g, Font f) {
+		String pageNum = (page + 1) + "/" + emo.pages;
+		String zoomN = "x" + zoom;
+		String prefetch = (emo.infoReady >= 0 && emo.infoReady < 100) ? ("fetching pages " + emo.infoReady + "%")
+				: null;
+		String ram;
+		{
+			long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+			used /= 1024;
+			if (used <= 4096) {
+				ram = used + "kb";
+			} else {
+				ram = (used / 1024) + "mb";
+			}
+		}
+		g.setGrayScale(0);
+		g.fillRect(0, 0, f.stringWidth(pageNum), f.getHeight());
+		g.fillRect(getWidth() - f.stringWidth(zoomN), 0, f.stringWidth(zoomN), f.getHeight());
+		if (prefetch != null)
+			g.fillRect(0, getHeight() - f.getHeight(), f.stringWidth(pageNum), f.getHeight());
+		g.fillRect(getWidth() - f.stringWidth(ram), getHeight() - f.getHeight(), f.stringWidth(ram), f.getHeight());
+
+		g.setGrayScale(255);
+		g.drawString(pageNum, 0, 0, 0);
+		g.drawString(zoomN, getWidth() - f.stringWidth(zoomN), 0, 0);
+		if (prefetch != null)
+			g.drawString(prefetch, 0, getHeight() - f.getHeight(), 0);
+		g.drawString(ram, getWidth(), getHeight(), Graphics.BOTTOM | Graphics.RIGHT);
+	}
+
+	private void limitOffset() {
+		int hw = toDraw.getWidth() / 2;
+		int hh = toDraw.getHeight() / 2;
+		if (x < -hw)
+			x = -hw;
+		if (x > hw)
+			x = hw;
+		if (y < -hh)
+			y = -hh;
+		if (y > hh)
+			y = hh;
+	}
+
+	private void paintNullImg(Graphics g, Font f) {
+		String info;
+		if (error) {
+			info = "Failed to load image.";
+		} else if (emo.infoReady == -1) {
+			info = "Failed to fetch pages.";
+		} else if (emo.infoReady == -2) {
+			info = "Waiting loader...";
+		} else {
+			info = origImg == null ? "Loading image..." : "Resizing...";
+		}
+		g.setGrayScale(255);
+		g.drawString(info, getWidth() / 2, getHeight() / 2, Graphics.HCENTER | Graphics.TOP);
+		if (hasPointerEvents()) {
+			int fh = f.getHeight();
+			// grads
+			fillGrad(g, getWidth() * 2 / 3, getHeight() - 50, getWidth() / 3, 51, 0, 0x222222);
+			// lines
+			g.setGrayScale(255);
+			g.drawLine(getWidth() * 2 / 3, getHeight() - 50, getWidth(), getHeight() - 50);
+			g.drawLine(getWidth() * 2 / 3, getHeight() - 50, getWidth() * 2 / 3, getHeight());
+			// captions
+			g.setGrayScale(255);
+			g.drawString(touchCaps[5], getWidth() * 5 / 6, getHeight() - 25 - fh / 2,
+					Graphics.TOP | Graphics.HCENTER);
+
 		}
 	}
 
@@ -251,6 +267,7 @@ public class View extends Canvas implements Runnable {
 	void reload() {
 		toDraw = null;
 		origImg = null;
+		System.gc();
 		loader = new Thread(this);
 		loader.start();
 	}
